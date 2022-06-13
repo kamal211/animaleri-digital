17df7f95d3c1ed09594d0f6e54f2855c379a641a
kamal211 <taouali.kamal.solicode@gmail.com>
2022-06-13 16:59:33 +0100

    first commit

---
+++ Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='hero'>
+      <div class="card bg-dark text-white border-0">
+  <img src="/images/banner1.png" class="card-img" alt="background "/>
+  <div class="card-img-overlay d-flex flex-column justify-content-center">
+      <div className="container">
+    <h5 class="card-title display-3 fw-bolder mb-0">Découvrir nos offres</h5>
+    <p class="card-text lead fs-2">Lorem ipsum dolor sit amet</p>
+</div>
+  </div>
+</div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ Product.jsx
@@ -0,0 +1,19 @@
+import React , {useState, useEffect} from 'react';
+
+const Product = () => {
+
+    const[data, setdata] = useState([]);
+    const[filter, setfilter] = useState(data);
+    const[loading, setloading] = useState(false);
+    let componentMounted = true;
+
+    
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Product;
