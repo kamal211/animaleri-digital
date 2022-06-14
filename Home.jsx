95d59fff6a82be907e289edf6db1133406bc4ca4
kamal211 <92024406+kamal211@users.noreply.github.com>
2022-06-14 14:21:52 -0700

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import Product from './Product';
 
 const Home = () => {
   return (
-    <div className='hero'>
-      <div class="card bg-dark text-white border-0">
-  <img src="/images/banner1.png" class="card-img" alt="background "/>
-  <div class="card-img-overlay d-flex flex-column justify-content-center">
-      <div className="container">
-    <h5 class="card-title display-3 fw-bolder mb-0">Découvrir nos offres</h5>
-    <p class="card-text lead fs-2">Lorem ipsum dolor sit amet</p>
+    <div className Name='hero'>
+      <div className ="card bg-dark text-white border-0">
+  <img src="/images/banner1.png" className ="card-img" alt="background "/>
+  <div className ="card-img-overlay d-flex flex-column justify-content-center">
+      <div className Name="container">
+    <h5 className ="card-title display-3 fw-bolder mb-0">Découvrir nos offres</h5>
+    <p className ="card-text lead fs-2">Lorem ipsum dolor sit amet</p>
 </div>
   </div>
 </div>
+<Product/>
     </div>
   );
 }
