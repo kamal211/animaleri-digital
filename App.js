906a247d05ed3d22f43196cbd99b57806b7aad66
kamal211 <92024406+kamal211@users.noreply.github.com>
2022-06-15 16:58:45 +0100

    Update App.js

--- App.js
+++ App.js
@@ -2,13 +2,20 @@
 import './App.css';
 import Home from './component/Home';
 import Navbar from './component/Navbar';
-
+import {Routes, Route} from 'react-router-dom';
+import Products from './component/Products';
+import Product from './component/Product';
 function App() {
   return (
-<>
-<Navbar />
-<Home />
-</>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path=":products" element={<Products/>}>
+          <Route path=":productid" element={<Product/>} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
